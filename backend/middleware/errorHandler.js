f61e57b992ef3e880e64d5e532cbcfd86c793922
Andrey SHevkenov <shevkenov@gmail.com>
2022-02-28 19:51:54 +0200

    fix: sending error status

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,6 +1,7 @@
 const errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode ? res.statusCode : 500;
-    res.status(statusCode);
+    //const statusCode = res.statusCode ? res.statusCode : 500;
+    //console.log(statusCode);
+    res.status(500);
     res.json({
         message: err.message,
         stack: process.env.NODE_ENV === 'PRODUCTION' ? null : err.stack
